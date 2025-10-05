de6fff05ff7d780ebd8d0d264d8296aece4b4ccc
social9009 <akshaysawant9009@gmail.com>
2025-10-06 00:33:53 +0530

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "my-terraform-Dev-2025-main-file-state"
+    bucket = "my-terraform-dev-2025-main-file-state"
     key = "main"
     region = "ap-south-1"
     dynamodb_table = "my-dynamo-table-2025"
